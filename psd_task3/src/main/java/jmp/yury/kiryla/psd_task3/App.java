8b2fc143430b75849264c57f3574d952b55f40a8
Yury Kiryla <yurykiryla@gmail.com>
2015-11-29 00:01:37 +0300

    Initial commit for task3

---
+++ psd_task3/src/main/java/jmp/yury/kiryla/psd_task3/App.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package jmp.yury.kiryla.psd_task3;
+
+/**
+ * Main class
+ * 
+ * @author Yury
+ *
+ */
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
