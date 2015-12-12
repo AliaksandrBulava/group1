a2037b17499542d14d78ee74f1e961e85440b3bb
Yury Kiryla <yurykiryla@gmail.com>
2015-12-12 14:00:05 +0300

    Add Abstract Factory project

---
+++ creational_patterns_task1/src/main/java/jmp/yury/kiryla/creational_patterns_task1/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jmp.yury.kiryla.creational_patterns_task1;
+
+/**
+ * @author Yury
+ *
+ */
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
