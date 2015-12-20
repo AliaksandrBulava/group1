377b1bf7f05054c4d67e23644e7beeea6d178bdb
Yury Kiryla <yurykiryla@gmail.com>
2015-12-20 23:04:15 +0300

    Add base GUI

--- structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/App.java
+++ structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/App.java
@@ -3,19 +3,55 @@
  */
 package jmp.yury.kiryla.structural_patterns_task1;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
 /**
  * Main class
  * 
  * @author Yury
  *
  */
-public class App {
+public class App extends Application {
+    
+    /**
+     * Title of view
+     */
+    private static final String VIEW_TITLE = "Records list";
+    
+    /**
+     * Window's width
+     */
+    private static final int WINDOW_WIDTH = 500;
+    
+    /**
+     * Window's height
+     */
+    private static final int WINDOW_HEIGHT = 300;
 
     /**
      * @param args
      */
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	launch(args);
+
+    }
+    
+    /**
+     * @see javafx.application.Application#start(javafx.stage.Stage)
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+	primaryStage.setTitle(VIEW_TITLE);
+	
+	FlowPane pane = new FlowPane();
+	
+	Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
+	primaryStage.setScene(scene);
+	
+	primaryStage.show();
 
     }
 
