19a7564738a8e525fab8585bb0b6a14875f9b01f
Yury Kiryla <yurykiryla@gmail.com>
2015-12-26 09:09:29 +0300

    update view and controller

--- structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/App.java
+++ structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/App.java
@@ -4,9 +4,14 @@
 package jmp.yury.kiryla.structural_patterns_task1;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import jmp.yury.kiryla.structural_patterns_task1.beans.Record;
+import jmp.yury.kiryla.structural_patterns_task1.controller.RecordsController;
 
 /**
  * Main class
@@ -16,20 +21,28 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
     
-    /**
-     * Title of view
-     */
+    /** Title of view */
     private static final String VIEW_TITLE = "Records list";
     
-    /**
-     * Window's width
-     */
+    /** Window's width */
     private static final int WINDOW_WIDTH = 500;
     
-    /**
-     * Window's height
-     */
+    /** Window's height */
     private static final int WINDOW_HEIGHT = 300;
+    
+    /** The width of the horizontal gaps between columns */
+    private static final int H_GAP = 15;
+    
+    /** The height of the vertical gaps between rows */
+    private static final int V_GAP = 10;
+    
+    /** Padding value */
+    private static final int PADDING = 25;
+    
+    /**
+     * {@link RecordsController}
+     */
+    private RecordsController recordsController;
 
     /**
      * @param args
@@ -40,19 +53,41 @@ public class App extends Application {
     }
     
     /**
+     * @see javafx.application.Application#init()
+     */
+    @Override
+    public void init() throws Exception {
+	super.init();
+	recordsController = new RecordsController();
+    }
+
+    /**
      * @see javafx.application.Application#start(javafx.stage.Stage)
      */
     @Override
     public void start(Stage primaryStage) throws Exception {
 	primaryStage.setTitle(VIEW_TITLE);
 	
-	FlowPane pane = new FlowPane();
+	//create pane
+	Pane pane = createPane();
 	
-	Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
-	primaryStage.setScene(scene);
+	//list view
+	ListView<Record> listView = new ListView<>(recordsController.getRecords());
+	
 	
 	primaryStage.show();
-
     }
-
+    
+    /**
+     * Create pane 
+     * @return {@link Pane} object
+     */
+    private Pane createPane(){
+	GridPane gridPane = new GridPane();
+	gridPane.setAlignment(Pos.CENTER);
+	gridPane.setHgap(H_GAP);
+	gridPane.setVgap(V_GAP);
+	gridPane.setPadding(new Insets(PADDING));
+	return gridPane;
+    }
 }
--- structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/controller/RecordsController.java
+++ structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/controller/RecordsController.java
@@ -3,6 +3,14 @@
  */
 package jmp.yury.kiryla.structural_patterns_task1.controller;
 
+import java.util.Arrays;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import jmp.yury.kiryla.structural_patterns_task1.beans.Record;
+import jmp.yury.kiryla.structural_patterns_task1.dao.RecordsDao;
+import jmp.yury.kiryla.structural_patterns_task1.dao.beans.SetRecordsDao;
+
 /**
  * Records Controller
  * 
@@ -10,5 +18,16 @@ package jmp.yury.kiryla.structural_patterns_task1.controller;
  *
  */
 public class RecordsController {
+    /**
+     * {@link RecordsDao}
+     */
+    private RecordsDao recordsDao = new SetRecordsDao();
     
+    /**
+     * Return records list
+     * @return the {@link ObservableList} object
+     */
+    public ObservableList<Record> getRecords(){
+	return FXCollections.observableArrayList(recordsDao.read());
+    }
 }
