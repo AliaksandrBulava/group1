b2a4c5e9a0095096c866f262b2f232ac581446d0
Yury Kiryla <yurykiryla@gmail.com>
2015-12-26 10:51:48 +0300

    add newRecord window

--- structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/App.java
+++ structural_patterns_task1/src/main/java/jmp/yury/kiryla/structural_patterns_task1/App.java
@@ -6,8 +6,12 @@ package jmp.yury.kiryla.structural_patterns_task1;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import jmp.yury.kiryla.structural_patterns_task1.beans.Record;
@@ -69,20 +73,45 @@ public class App extends Application {
 	primaryStage.setTitle(VIEW_TITLE);
 	
 	//create pane
-	Pane pane = createPane();
+	GridPane gridPane = createPane();
+	
+	//Scene
+	Scene scene = new Scene(gridPane, WINDOW_WIDTH, WINDOW_HEIGHT);
+	primaryStage.setScene(scene);
 	
 	//list view
 	ListView<Record> listView = new ListView<>(recordsController.getRecords());
+	gridPane.add(listView, 1, 1);
 	
+	//Buttons
+	Button newButton = new Button("New");
+	Button deleteButton = new Button("Delete");
+	HBox buttonsBox = new HBox(10);
+	buttonsBox.getChildren().addAll(newButton, deleteButton);
+	gridPane.add(buttonsBox, 1, 2);
+	
+	//add new Record
+	newButton.setOnAction(ae -> {
+	    Stage newRecordStage = new Stage();
+	    newRecordStage.setTitle("Add new Record");
+	    
+	    FlowPane newRecordFlowPane = new FlowPane(10, 10);
+	    newRecordFlowPane.setAlignment(Pos.CENTER);
+	    
+	    Scene newRecordScene = new Scene(newRecordFlowPane, 350, 150);
+	    newRecordStage.setScene(newRecordScene);
+	    
+	    newRecordStage.show();
+	});
 	
 	primaryStage.show();
     }
     
     /**
      * Create pane 
-     * @return {@link Pane} object
+     * @return {@link GridPane} object
      */
-    private Pane createPane(){
+    private GridPane createPane(){
 	GridPane gridPane = new GridPane();
 	gridPane.setAlignment(Pos.CENTER);
 	gridPane.setHgap(H_GAP);
