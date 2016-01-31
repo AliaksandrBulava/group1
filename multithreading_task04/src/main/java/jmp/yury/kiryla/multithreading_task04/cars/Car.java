87690fd714f98a3e538c7a1dcedf37d056a16091
Yury Kiryla <yurykiryla@gmail.com>
2016-01-31 19:38:06 +0300

    Add Car class

---
+++ multithreading_task04/src/main/java/jmp/yury/kiryla/multithreading_task04/cars/Car.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package jmp.yury.kiryla.multithreading_task04.cars;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Yury
+ *
+ */
+public class Car implements Runnable {
+    private static final long MAX_DISTANCE = 10000;
+
+    Logger log = Logger.getLogger(getClass());
+    
+    private long friction; 
+    private long distance; 
+    
+    private String name;
+    
+    public Car(String name, long friction) {
+        this.name = name;
+        this.friction = 100;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (distance < MAX_DISTANCE) {
+                Thread.sleep(friction);
+                distance += 100;
+                log.info(name + " " + distance);
+            }
+        } catch (InterruptedException e) {
+            log.error(e);
+        }
+    }
+}
