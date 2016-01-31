8ebab1993c6d4f42a5cc24b8e6c25dac82ef1f50
Yury Kiryla <yurykiryla@gmail.com>
2016-01-31 10:44:04 +0300

    Initial commit for 'Thread Default Task 04. Parallel Threads'

---
+++ multithreading_task04/src/main/java/jmp/yury/kiryla/multithreading_task04/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jmp.yury.kiryla.multithreading_task04;
+
+/**
+ * @author Yury
+ *
+ */
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
