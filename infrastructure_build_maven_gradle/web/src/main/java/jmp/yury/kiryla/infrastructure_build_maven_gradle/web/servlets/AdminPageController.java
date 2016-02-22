dc7ccb85bfed1edcf59952db4bd559d812c6324f
Yury Kiryla <yury_kiryla@epam.com>
2016-02-22 14:42:13 +0300

    add admin page controller

---
+++ infrastructure_build_maven_gradle/web/src/main/java/jmp/yury/kiryla/infrastructure_build_maven_gradle/web/servlets/AdminPageController.java
@@ -0,0 +1,43 @@
+package jmp.yury.kiryla.infrastructure_build_maven_gradle.web.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jmp.yury.kiryla.infrastructure_build_maven_gradle.services.AuditoriumService;
+import jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService;
+
+/**
+ * Servlet implementation class AdminPageController
+ */
+@WebServlet("/admin")
+public class AdminPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminPageController() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EventService eventService = (EventService) getServletContext().getAttribute("eventService");
+		request.setAttribute("events", eventService.getAll());
+		response.sendRedirect(request.getContextPath() + "/jsp/admin.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ infrastructure_build_maven_gradle/web/src/main/webapp/jsp/admin.jsp
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Admin</title>
+	</head> 
+	<body>
+		<c:if test="${not empty events}">
+			<table>
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Auditorium</th>
+						<th>Date</th>
+						<th>Start Time</th>
+						<th>End Time</th>
+					</tr>
+				</thead>
+				<tbody>
+					<c:forEach var="event" items="events">
+						<tr>
+							<td>event.name</td>
+							<td>event.auditorium.name</td>
+							<td>event.date</td>
+							<td>event.startTime</td>
+							<td>event.endTime</td>
+						</tr>
+					</c:forEach>
+				</tbody>
+			</table>
+		</c:if>
+		
+		<c:url value="/admin/event" var="eventUrl" />
+		<a href="${adminUrl}">Add Event</a>
+		<c:url value="/admin/auditorium" var="userUrl" />
+		<a href="${userUrl}">Add Auditorium</a>
+	</body>
+</html>
