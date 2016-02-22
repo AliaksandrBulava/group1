df3968531a546790bc1b14b86c945aecc0d5d2fb
Yury Kiryla <yury_kiryla@epam.com>
2016-02-22 16:39:05 +0300

    Event page update

--- infrastructure_build_maven_gradle/web/src/main/java/jmp/yury/kiryla/infrastructure_build_maven_gradle/web/servlets/EventPageController.java
+++ infrastructure_build_maven_gradle/web/src/main/java/jmp/yury/kiryla/infrastructure_build_maven_gradle/web/servlets/EventPageController.java
@@ -1,6 +1,8 @@
 package jmp.yury.kiryla.infrastructure_build_maven_gradle.web.servlets;
 
 import java.io.IOException;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import jmp.yury.kiryla.infrastructure_build_maven_gradle.services.AuditoriumService;
+import jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService;
 import jmp.yury.kiryla.infrastructure_build_maven_gradle.web.Constants;
 
 /**
@@ -16,6 +19,16 @@ import jmp.yury.kiryla.infrastructure_build_maven_gradle.web.Constants;
 @WebServlet("/admin/event")
 public class EventPageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * {@link AuditoriumService}
+	 */
+	private AuditoriumService auditoriumService;
+	
+	/**
+	 * {@link EventService}
+	 */
+	private EventService eventService;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -24,11 +37,24 @@ public class EventPageController extends HttpServlet {
         super();
     }
 
+    
+    
+	/**
+	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		auditoriumService = (AuditoriumService) config.getServletContext().getAttribute(Constants.AUDITORIUM_SERVICE);
+		eventService = (EventService) config.getServletContext().getAttribute(Constants.EVENT_SERVICE);
+	}
+
+
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		AuditoriumService auditoriumService = (AuditoriumService) getServletContext().getAttribute(Constants.AUDITORIUM_SERVICE);
 		request.setAttribute("auditoriums", auditoriumService.getAll());
 		
 		response.sendRedirect(request.getContextPath() + "/jsp/event.jsp");
@@ -38,8 +64,13 @@ public class EventPageController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String name = request.getParameter("eventName");
+		String auditoriumString = request.getParameter("eventAuiditorium");
+		String dateString = request.getParameter("eventDate");
+		String startTimeString = request.getParameter("eventStartTime");
+		String endTimeString = request.getParameter("eventEndTime");
+		
+		response.sendRedirect(request.getContextPath() + "/jsp/event.jsp");
 	}
 
 }
