5f4504b5214255eb32617040a1d58db1a4f10e67
Yury Kiryla <yury_kiryla@epam.com>
2016-02-22 14:21:44 +0300

    update event servoce implementation

--- infrastructure_build_maven_gradle/services/src/main/java/jmp/yury/kiryla/infrastructure_build_maven_gradle/services/beans/EventServiceBean.java
+++ infrastructure_build_maven_gradle/services/src/main/java/jmp/yury/kiryla/infrastructure_build_maven_gradle/services/beans/EventServiceBean.java
@@ -19,81 +19,91 @@ import jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService;
  *
  */
 public class EventServiceBean implements EventService {
-    /**
-     * {@link EventDAO}
-     */
-    private EventDAO eventDAO;
+	/**
+	 * {@link EventDAO}
+	 */
+	private EventDAO eventDAO;
 
-    /**
-     * @param eventDAO
-     */
-    public EventServiceBean(EventDAO eventDAO) {
-	super();
-	this.eventDAO = eventDAO;
-    }
-
-    /**
-     * @see jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService#register(java.lang.String,
-     *      jmp.yury.kiryla.infrastructure_build_maven_gradle.beans.Auditorium,
-     *      java.time.LocalDate, java.time.LocalTime, java.time.LocalTime)
-     */
-    @Override
-    public Event register(String name, Auditorium auditorium, LocalDate date, LocalTime startTime, LocalTime endTime) {
-	if (name != null && !name.isEmpty() && auditorium != null && date != null && startTime != null
-		&& endTime != null && isAuditoriumFree(auditorium, date, startTime, endTime)) {
-	    Event event = new Event();
-
-	    event.setName(name);
-	    event.setAuditorium(auditorium);
-	    event.setDate(date);
-	    event.setStartTime(startTime);
-	    event.setEndTime(endTime);
-
-	    eventDAO.saveEvent(event);
-
-	    return event;
-
+	/**
+	 * @param eventDAO
+	 */
+	public EventServiceBean(EventDAO eventDAO) {
+		super();
+		this.eventDAO = eventDAO;
 	}
-	return null;
-    }
 
-    /**
-     * @see jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService#
-     *      delete(jmp.yury.kiryla.infrastructure_build_maven_gradle.beans.Event)
-     */
-    @Override
-    public void delete(Event event) {
-	if (event != null) {
-	    eventDAO.removeEvent(event);
-	}
-    }
+	/**
+	 * @see jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService#register(java.lang.String,
+	 *      jmp.yury.kiryla.infrastructure_build_maven_gradle.beans.Auditorium,
+	 *      java.time.LocalDate, java.time.LocalTime, java.time.LocalTime)
+	 */
+	@Override
+	public Event register(String name, Auditorium auditorium, LocalDate date, LocalTime startTime, LocalTime endTime) {
+		if (name != null && !name.isEmpty() && auditorium != null && date != null && startTime != null
+				&& endTime != null && isAuditoriumFree(auditorium, date, startTime, endTime)) {
+			Event event = new Event();
 
-    /**
-     * Check if auditorium is free on requested date-time
-     * 
-     * @param auditorium
-     *            {@link Auditorium}
-     * @param date
-     *            Date
-     * @param startTime
-     *            Start Time
-     * @param endTime
-     *            End Time
-     * @return <code>true</code> if auditorium available
-     */
-    private boolean isAuditoriumFree(Auditorium auditorium, LocalDate date, LocalTime startTime, LocalTime endTime) {
-	List<Event> events = eventDAO.getEvents();
-	if (events != null) {
-	    return !events.stream().anyMatch(event -> {
-		if (event.getDate().isEqual(date)
-			&& (startTime.isAfter(event.getStartTime()) && startTime.isBefore(event.getEndTime())
-				|| (endTime.isAfter(event.getStartTime()) && endTime.isBefore(event.getEndTime())))) {
-		    return true;
+			event.setName(name);
+			event.setAuditorium(auditorium);
+			event.setDate(date);
+			event.setStartTime(startTime);
+			event.setEndTime(endTime);
+
+			eventDAO.saveEvent(event);
+
+			return event;
+
 		}
-		return false;
-	    });
-	} else {
-	    return false;
+		return null;
 	}
-    }
+
+	/**
+	 * @see jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService#
+	 *      delete(jmp.yury.kiryla.infrastructure_build_maven_gradle.beans.Event)
+	 */
+	@Override
+	public void delete(Event event) {
+		if (event != null) {
+			eventDAO.removeEvent(event);
+		}
+	}
+
+	/**
+	 * Check if auditorium is free on requested date-time
+	 * 
+	 * @param auditorium
+	 *            {@link Auditorium}
+	 * @param date
+	 *            Date
+	 * @param startTime
+	 *            Start Time
+	 * @param endTime
+	 *            End Time
+	 * @return <code>true</code> if auditorium available
+	 */
+	private boolean isAuditoriumFree(Auditorium auditorium, LocalDate date, LocalTime startTime, LocalTime endTime) {
+		List<Event> events = eventDAO.getEvents();
+		if (events != null) {
+			return !events.stream().anyMatch(event -> {
+				if (event.getDate().isEqual(date)
+						&& (startTime.isAfter(event.getStartTime()) && startTime.isBefore(event.getEndTime())
+								|| (endTime.isAfter(event.getStartTime()) && endTime.isBefore(event.getEndTime())))) {
+					return true;
+				}
+				return false;
+			});
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * @see jmp.yury.kiryla.infrastructure_build_maven_gradle.services.EventService#getAll()
+	 */
+	@Override
+	public List<Event> getAll() {
+		return eventDAO.getEvents();
+	}
+	
+	
 }
