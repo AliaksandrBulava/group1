6e60bb1aa7dc14ccaaa54db42b14b6a7bf58fa1f
Yury Kiryla <yury_kiryla@epam.com>
2016-03-18 15:41:36 +0300

    add user dao

---
+++ web_services_task1/rest_service/src/main/java/jmp/yury/kiryla/web_services_task1/service/dao/UserDAO.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package jmp.yury.kiryla.web_services_task1.service.dao;
+
+import java.util.List;
+
+import jmp.yury.kiryla.web_services_task1.beans.User;
+
+/**
+ * User DAO
+ * 
+ * @author Yury_Kiryla
+ *
+ */
+public interface UserDAO {
+	/**
+	 * Create new User
+	 * @param user the {@link User} object
+	 */
+	public void create(User user);
+	
+	/**
+	 * Get User by Id
+	 * @param id ID
+	 */
+	public User getUserById(long id);
+	
+	/**
+	 * Get Users by name
+	 * @param name User's name
+	 * @return {@link User}s list
+	 */
+	public List<User> getUsersByName(String name);
+	
+	/**
+	 * Get All Users
+	 * @return {@link User}s list
+	 */
+	public List<User> getAll();
+	
+	/**
+	 * Update existing User
+	 * @param user the {@link User} object
+	 */
+	public void update(User user);
+	
+	/**
+	 * Delete existing User
+	 * @param user the {@link User} object
+	 */
+	public void delete(User user);
+}
