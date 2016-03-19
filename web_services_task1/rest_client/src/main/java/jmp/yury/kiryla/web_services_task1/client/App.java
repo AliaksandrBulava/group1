a78b65b7d9eeba3336edd8e1dde19cdf9a563bc6
Yury Kiryla <yurykiryla@gmail.com>
2016-03-19 22:21:00 +0300

    add rest client main class

---
+++ web_services_task1/rest_client/src/main/java/jmp/yury/kiryla/web_services_task1/client/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jmp.yury.kiryla.web_services_task1.client;
+
+/**
+ * @author Yury
+ *
+ */
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+}
