10464dbd3e0dd162a1e540e0b1ee7416cab59149
Yury Kiryla <yury_kiryla@epam.com>
2016-03-23 16:07:41 +0300

    init jpa project

---
+++ jpa_task1/src/main/java/jmp/yury/kiryla/jpa_task1/App.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package jmp.yury.kiryla.jpa_task1;
+
+/**
+ * Main class for application
+ * 
+ * @author Yury_Kiryla
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
