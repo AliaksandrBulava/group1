2eedc5de99e58cdd2836e0239293d8b66595c2c2
Yury Kiryla <yury_kiryla@epam.com>
2016-04-01 19:22:02 +0300

    add Repository configuration

--- spring_core/src/main/java/org/shop/ShopLauncher.java
+++ spring_core/src/main/java/org/shop/ShopLauncher.java
@@ -1,5 +1,7 @@
 package org.shop;
 
+import org.shop.data.Item;
+import org.shop.repository.ItemRepository;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,7 +17,9 @@ public class ShopLauncher {
      */
     public static void main(String[] args) {
         try (ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("spring-context.xml")) {
-        	System.out.println(ctx.getDisplayName());
+        	
+        	ItemRepository itemRepository = ctx.getBean("itemRepository", ItemRepository.class);
+        	System.out.println(itemRepository.createItem(new Item()));
         }
     }
 }
---
+++ spring_core/src/main/java/org/shop/config/RepositoryCongiguration.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package org.shop.config;
+
+import org.shop.repository.ItemRepository;
+import org.shop.repository.OrderRepository;
+import org.shop.repository.ProductRepository;
+import org.shop.repository.ProposalRepository;
+import org.shop.repository.SellerRepository;
+import org.shop.repository.UserRepository;
+import org.shop.repository.factory.UserRepositoryFactory;
+import org.shop.repository.map.ItemMapRepository;
+import org.shop.repository.map.OrderMapRepository;
+import org.shop.repository.map.ProductMapRepository;
+import org.shop.repository.map.ProposalMapRepository;
+import org.shop.repository.map.SellerMapRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Repositories Configuration
+ * 
+ * @author Yury_Kiryla
+ *
+ */
+@Configuration
+public class RepositoryCongiguration {
+	/**
+	 * {@link ItemRepository}
+	 * @return {@link ItemRepository} object
+	 */
+	@Bean
+	public ItemRepository itemRepository() {
+		return new ItemMapRepository();
+	}
+
+	/**
+	 * {@link OrderRepository}
+	 * @return {@link OrderRepository} object
+	 */
+	@Bean
+	public OrderRepository orderRepository() {
+		return new OrderMapRepository(1);
+	}
+	
+	/**
+	 * {@link ProductRepository}
+	 * @return {@link ProductRepository} object
+	 */
+	@Bean
+	public ProductRepository productRepository() {
+		return new ProductMapRepository();
+	}
+	
+	/**
+	 * {@link ProposalRepository}
+	 * @return {@link ProposalRepository} object
+	 */
+	@Bean
+	public ProposalRepository proposalRepository() {
+		return new ProposalMapRepository();
+	}
+	
+	/**
+	 * {@link SellerRepository}
+	 * @return {@link SellerRepository} object
+	 */
+	@Bean
+	public SellerRepository sellerRepository() {
+		return new SellerMapRepository();
+	}
+	
+	/**
+	 * {@link UserRepository}
+	 * @return {@link UserRepository} object
+	 */
+	@Bean
+	public UserRepository userRepository() {
+		return new UserRepositoryFactory().createUserRepository();
+	}
+}
