1d81d0ddc73f4fe880ab007e12299a1f3526dbcf
Yury Kiryla <yurykiryla@gmail.com>
2016-04-03 15:21:22 +0300

    add services configuration

---
+++ spring_core/src/main/java/org/shop/config/ServicesConfig.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.shop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Services Configuration
+ * 
+ * @author Yury
+ *
+ */
+@Configuration
+@ComponentScan("org.shop.api")
+public class ServicesConfig {
+
+}
