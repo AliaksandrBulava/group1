27012b6a4b2e5c4bc42c39d120a33a4c8f5d301a
Yury Kiryla <yurykiryla@gmail.com>
2016-04-03 15:28:17 +0300

    add initializers configuration

---
+++ spring_core/src/main/java/org/shop/config/InitializersConfig.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.shop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Initializers Configuration
+ * 
+ * @author Yury
+ *
+ */
+@Configuration
+@ComponentScan("org.shop")
+public class InitializersConfig {
+
+}
