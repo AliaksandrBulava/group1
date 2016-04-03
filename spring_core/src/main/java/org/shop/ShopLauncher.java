80ee7f87fc1bdf28bbd95523dc69f322ee1d8b19
Yury Kiryla <yurykiryla@gmail.com>
2016-04-03 15:50:46 +0300

    add repositories

--- spring_core/src/main/java/org/shop/ShopLauncher.java
+++ spring_core/src/main/java/org/shop/ShopLauncher.java
@@ -1,17 +1,26 @@
 package org.shop;
 
+import org.shop.config.RootConfig;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * The ShopLauncher class.
  */
 public class ShopLauncher {
-    
-    /**
-     * The main method.
-     *
-     * @param args the arguments
-     */
-    public static void main(String[] args) {
-        //TODO: implement using Spring Framework ApplicationContext
-    }
+
+	/**
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(RootConfig.class)) {
+			Object bean = ctx.getBean("userRepository");
+			System.out.println(bean);
+			bean = ctx.getBean("itemRepository");
+			System.out.println(bean);
+		}
+	}
 }
