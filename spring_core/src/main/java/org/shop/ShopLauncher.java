f3b6a5f17a3c60abe35e0e1d312ba170e7a56f37
Yury Kiryla <yury_kiryla@epam.com>
2016-04-04 19:17:38 +0300

    add test

--- spring_core/src/main/java/org/shop/ShopLauncher.java
+++ spring_core/src/main/java/org/shop/ShopLauncher.java
@@ -1,6 +1,14 @@
 package org.shop;
 
+
+import org.shop.api.ItemService;
+import org.shop.api.OrderService;
+import org.shop.api.ProductService;
+import org.shop.api.ProposalService;
+import org.shop.api.UserService;
 import org.shop.config.RootConfig;
+import org.shop.data.Product;
+import org.shop.data.User;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -17,46 +25,35 @@ public class ShopLauncher {
 	 */
 	public static void main(String[] args) {
 		try (ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(RootConfig.class)) {
-			Object bean = ctx.getBean("userRepository");
-			System.out.println(bean);
-			bean = ctx.getBean("itemRepository");
-			System.out.println(bean);
-			bean = ctx.getBean("orderRepository");
-			System.out.println(bean);
-			bean = ctx.getBean("productRepository");
-			System.out.println(bean);
-			bean = ctx.getBean("proposalRepository");
-			System.out.println(bean);
-			bean = ctx.getBean("sellerRepository");
-			System.out.println(bean);
 			
-			bean = ctx.getBean("orderService");
-			System.out.println(bean);
-			bean = ctx.getBean("itemService");
-			System.out.println(bean);
-			bean = ctx.getBean("sellerService");
-			System.out.println(bean);
-			bean = ctx.getBean("productService");
-			System.out.println(bean);
-			bean = ctx.getBean("userService");
-			System.out.println(bean);
-			bean = ctx.getBean("proposalService");
-			System.out.println(bean);
+			//Get bean by name
+			ItemService itemService = (ItemService) ctx.getBean("itemService");
 			
-			bean = ctx.getBean("sellerInitializer");
-			System.out.println(bean);
-			bean = ctx.getBean("proposalInitializer");
-			System.out.println(bean);
-			bean = ctx.getBean("productInitializer");
-			System.out.println(bean);
-			bean = ctx.getBean("dataInitializer");
-			System.out.println(bean);
+			// Get Bean by Type
+			OrderService orderService = ctx.getBean(OrderService.class);
 			
-			bean = ctx.getBean("u_serv");
-			System.out.println(bean);
+			// Get bean by type and name
+			ProductService productService = ctx.getBean("productService", ProductService.class);
+
+			// Get bean by alias
+			UserService userService = (UserService) ctx.getBean("u_serv");
 			
+			//Check if it works
 			
+			Product product = new Product();
+			product.setName("N450");
+			product.setDescription("Laptop");
+			productService.createProduct(product);
+			System.out.println("Product created: " + product);
 			
+			User user = new User();
+			user.setUsername("John");
+			userService.registerUser(user);
+			System.out.println("User registered: " + user);
+			
+			ProposalService proposalService = ctx.getBean(ProposalService.class);
+			orderService.createOrder(user, proposalService.getProposalsByProductId(1l).get(0));
+			System.out.println("Created order: " + orderService.getOrdersByUser(user).get(0));
 		}
 	}
 }
